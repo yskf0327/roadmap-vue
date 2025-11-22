f55ca2b5c538075a4a1fcfc780ee7a8ffdb02c84
Yusuke <ysk2645j4@gmail.com>
2025-11-22 14:32:54 +0900

    style:SP版基本レイアウト作成

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div>
-    <h1>roadmap</h1>
+    <header class="page-header">
+      <div class="page-header__inner container">
+        <hgroup class="page-title">
+          <p>2025年11月開講 Webプログラミング科</p>
+          <h1>授業ロードマップ</h1>
+        </hgroup>
+      </div>
+    </header>
 
     <div class="container">
       <ol class="roadmap">
-        <RoadmapPhase v-for="(phase,index) in phases" :key="index" :phase="phase"></RoadmapPhase>
+        <RoadmapPhase v-for="(phase, index) in phases" :key="index" :phase="phase"></RoadmapPhase>
       </ol>
     </div>
   </div>
 </template>
 
 <script setup>
-
-import phases from './data/radmap.json'
-import RoadmapPhase from './components/RoadmapPhase.vue';
-
+import phases from './data/roadmap.json'
+import RoadmapPhase from './components/RoadmapPhase.vue'
+console.log(phases)
 </script>
 
 <style scoped>
-
+.roadmap {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
 </style>
--- src/components/RoadmapPhase.vue
+++ src/components/RoadmapPhase.vue
@@ -1,11 +1,12 @@
 <template>
   <li class="roadmap-phase">
-    <h2 class="roadmap-phase__title">{{ phase.title }}</h2>
-
-    <p class="roadmap-phase__period">
-      <time :datetime="phase.start">{{ phase.start }}</time> -
-      <time :datetime="phase.end">{{ phase.end }}</time>
-    </p>
+    <header class="roadmap-phase__header">
+      <h2 class="roadmap-phase__title"><span>{{ phase.title }}</span></h2>
+      <p class="roadmap-phase__period">
+        <time :datetime="phase.start">{{ formatDate(phase.start) }}</time> -
+        <time :datetime="phase.end">{{ formatDate(phase.end) }}</time>
+      </p>
+    </header>
 
     <div class="roadmap-phase__content">
       <ul>
@@ -14,7 +15,9 @@
         </li>
       </ul>
 
-      <p v-if="phase.note">{{ phase.note }}</p>
+      <div class="roadmap-phase__note">
+        <p v-if="phase.note" v-for="(para, index) in phase.note" :key="index">{{ para }}</p>
+      </div>
     </div>
   </li>
 </template>
@@ -26,8 +29,105 @@ defineProps({
     required: true
   }
 });
+
+const formatDate = (datetime) => {
+  const sep = '.';
+  if (!datetime) return '';
+  const dateObj = new Date(datetime);
+  const y = dateObj.getFullYear();
+  const n = dateObj.getMonth() + 1;
+  const j = dateObj.getDate();
+
+  return `${y}.${n}.${j}`;
+}
 </script>
 
 <style scoped>
-.roadmap-phase { background: var(--color-primary); color: var(--color-background); clip-path: polygon(100% 0, 100% 85%, 50% 100%, 0 85%, 0 0, 50% 15%); padding: 55px; } 
+.roadmap-phase {
+  display: flex;
+  flex-direction: column;
+  gap: 21px;
+  background: var(--color-primary);
+  color: var(--color-background);
+  /* clip-path: polygon(100% 0, 100% 90%, 50% 100%, 0 90%, 0 0, 50% 10%); */
+  padding-inline: var(--s-md);
+  padding-block: var(--s-2xl) var(--s-xl);
+  position: relative;
+
+  &::before,
+  &::after {
+    position: absolute;
+    left: 0;
+    display: block;
+    content: "";
+    width: 100%;
+    height: 30px;
+    clip-path: polygon(50% 100%, 0 1%, 100% 1%);
+  }
+
+  &::before {
+    top: -1px;
+    background-color: var(--color-background);
+  }
+
+  &::after {
+    bottom: -29px;
+    background-color: var(--color-primary);
+  }
+}
+
+.roadmap-phase__title {
+  /* font-size: var(--fz-lg); */
+  font-weight: bold;
+
+  span {
+    background-image: linear-gradient(180deg, transparent 70%, var(--color-secondary) 20%);
+  }
+}
+
+.roadmap-phase__period {
+  font-size: var(--fz-base);
+  margin-block-start: var(--s-sm);
+
+}
+
+.roadmap-phase__content {
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+
+  ul>li,
+  p {
+    line-height: 1.7;
+  }
+
+
+
+  ul {
+    margin-inline-start: 0.5em;
+
+    li {
+      padding-inline-start: 0.3em;
+
+      &::marker {
+        content: "-";
+      }
+    }
+  }
+}
+
+.roadmap-phase__note {
+  background-color: var(--color-text);
+  /* color: var(--color-text); */
+  border-radius: var(--s-sm);
+  padding: var(--s-sm);
+
+  p+p {
+    margin-block-start: var(--s-md);
+  }
+}
+
+/* .roadmap-phase__content p {
+  white-space: pre-line;
+} */
 </style>
