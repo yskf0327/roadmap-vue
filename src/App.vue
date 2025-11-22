79d68f171cc8af0e3ee9c37008f803101f0bef75
Yusuke <ysk2645j4@gmail.com>
2025-11-22 11:25:47 +0900

    feat:初期コミット

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>roadmap</h1>
+
+    <div class="container">
+      <ol class="roadmap">
+        <RoadmapPhase v-for="(phase,index) in phases" :key="index" :phase="phase"></RoadmapPhase>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import 'destyle.css' 
+import phases from './data/radmap.json'
+import RoadmapPhase from './components/RoadmapPhase.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/RoadmapPhase.vue
@@ -0,0 +1,33 @@
+<template>
+  <li class="roadmap-phase">
+    <h2 class="roadmap-phase__title">{{ phase.title }}</h2>
+
+    <p class="roadmap-phase__period">
+      <time :datetime="phase.start">{{ phase.start }}</time> -
+      <time :datetime="phase.end">{{ phase.end }}</time>
+    </p>
+
+    <div class="roadmap-phase__content">
+      <ul>
+        <li v-for="(item, index) in phase.items" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+
+      <p v-if="phase.note">{{ phase.note }}</p>
+    </div>
+  </li>
+</template>
+
+<script setup>
+defineProps({
+  phase: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+.roadmap-phase { background: var(--color-primary); color: var(--color-background); clip-path: polygon(100% 0, 100% 85%, 50% 100%, 0 85%, 0 0, 50% 15%); padding: 55px; } 
+</style>
